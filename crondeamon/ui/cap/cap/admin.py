7f903c280c08a000f2b08f92ddf4ba24f186d121
周坤朋 <18749679769@163.com>
2016-12-01 15:10:43 +0800

    bug处理

--- crondeamon/ui/cap/cap/admin.py
+++ crondeamon/ui/cap/cap/admin.py
@@ -1,5 +1,4 @@
 __author__ = 'zhou'
 from django.contrib import  admin
-from models import Project
-from cron.models import App,CronServe
-admin.site.register([CronServe,Project,App])
+from cron.models import CronServe
+admin.site.register([CronServe])
