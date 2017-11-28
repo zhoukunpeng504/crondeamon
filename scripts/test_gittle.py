1f6cd72c4fcbecf307342d242c9a8a105af27251
zhoukunpeng504 <18749679769@163.com>
2017-11-28 14:14:54 +0800

    change readme

---
+++ scripts/test_gittle.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+# write  by  zhou
+
+#from gittle import  Gittle,GittleAuth
+
+#auth=GittleAuth(username="zhoukunpeng",password="gc895316")
+#repo=Gittle.clone("https://zhoukunpeng%40gongchang.com:gc895316@git.coding.net/zhoukunpeng/djangostudy.git","~/testgittless/")
