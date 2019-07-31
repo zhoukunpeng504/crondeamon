402f9533b632dab6f1c276d04219009b646eb63c
zhoukunpeng <gc895316>
2019-07-31 22:39:02 +0800

    ceshi github speed

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-#coding:utf-8
-__author__ = 'admin'
+# coding:utf-8
+__author__ = 'zhoukunpeng'
 # --------------------------------
-# Created by admin  on 2016/11/11.
+# Created by  zhoukunpeng  on 2016/11/11.
 # ---------------------------------
 
-from setuptools import  setup,find_packages
+from setuptools import setup, find_packages
+
 setup(
     name="crondeamon",
-    packages=list(set(list(find_packages())+["twisted.plugins"])),
+    packages=list(set(list(find_packages()) + ["twisted.plugins"])),
     version='0.1.4',
-    install_requires=["setuptools", "MySQL-python","twisted==15.3.0","txscheduling","psutil","django==1.4.22"],
+    install_requires=["setuptools", "MySQL-python", "twisted==15.3.0", "txscheduling", "psutil", "django==1.4.22"],
     include_package_data=True
 )
